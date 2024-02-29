d0522488a6177c38e83ad1c4c792d6c2d87802e4
PathMan21 <mlafosse@lilyfacilitelavie.com>
2024-02-29 10:04:22 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, InputGroup, FormControl, Button, Row, Card, CardBody, CardTitle, CardSubtitle } from "react-bootstrap";
+import { useState, useEffect } from 'react';
+import Login from './pages/login';
+import Callback from "./pages/callback";
+import Album from "./pages/main";
+import Anon from "./pages/mainAnon";
+import Register from "./pages/inscription";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+const code = new URLSearchParams(window.location.search).get("code");
+
+const router = createBrowserRouter([
+  {
+    path: "/anon",
+    element: <Anon/>
+  },
+  {
+    path: "/inscription",
+    element: <Register/>
+  },
+  {
+    path: "/",
+    element: <Login/>
+  },
+  {
+    path: "/album",
+    element: <Album/>
+
+  },
+  {
+    path: "/callback", 
+    element: <Callback/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  }
+
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return code ? <Callback/> : (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
